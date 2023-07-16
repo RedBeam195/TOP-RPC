1689c29b242913cd54ce463dce33cc27660035c0
RedBeam195 <shadowpilar195@gmail.com>
2023-07-16 20:32:47 +0200

    added GUI

--- index.js
+++ index.js
@@ -4,62 +4,136 @@ let getComputerChoice = () => {
 };
 let playerScore = 0;
 let machineScore = 0;
+
+let playerChoice = "";
+let gameFinished = false;
+
+let updateDisplay = (playerImg, machineImg) => {
+  let playerIcon = document.querySelector("#player-image");
+  let machineIcon = document.querySelector("#machine-image");
+
+  if (playerImg != "nothing") {
+    playerIcon.setAttribute("src", `./img/${playerImg}.png`);
+    if (machineImg != "nothing") {
+      machineIcon.setAttribute("src", `./img/${machineImg}.png`);
+    }
+  } else {
+    playerIcon.removeAttribute("src");
+    machineIcon.removeAttribute("src");
+  }
+}
+
 let playRound = (playerSelection, computerSelection) => {
   if (playerSelection === "rock") {
     if (computerSelection === "scissors") {
       console.log("You win, rock beats scissors");
-      playerScore+=1;
+      playerScore += 1;
+      updateScore()
+      updateDisplay(playerSelection, computerSelection);
+
     } else if (computerSelection === "paper") {
       console.log("You lose, paper wins rock");
-      machineScore+=1;
+      machineScore += 1;
+      updateScore()
+      updateDisplay(playerSelection, computerSelection);
+
+
     } else {
       console.log("Tie");
+      updateDisplay(playerSelection, computerSelection);
     }
   }
   if (playerSelection === "paper") {
     if (computerSelection === "scissors") {
       console.log("You lose, scissors beats paper");
-      machineScore+=1;
+      machineScore += 1;
+      updateScore()
+      updateDisplay(playerSelection, computerSelection);
+
+
     } else if (computerSelection === "rock") {
       console.log("You win, paper wins rock");
-      playerScore+=1;
+      playerScore += 1;
+      updateScore()
+      updateDisplay(playerSelection, computerSelection);
+
+
     } else {
       console.log("Tie");
+      updateDisplay(playerSelection, computerSelection);
+
     }
   }
   if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
       console.log("You lose, rock beats scissors");
-      machineScore+=1;
+      machineScore += 1;
+      updateScore()
+      updateDisplay(playerSelection, computerSelection);
+
+
     } else if (computerSelection === "paper") {
       console.log("You win, scissors wins paper");
-      playerScore+=1;
+      playerScore += 1;
+      updateScore()
+      updateDisplay(playerSelection, computerSelection);
+
+
     } else {
       console.log("Tie");
+      updateDisplay(playerSelection, computerSelection);
+
     }
   }
 };
 
-let startRound = () => {
-  playRound(prompt("Rock,Paper,Scissors").toLowerCase(), getComputerChoice());
-};
+let updateScore = () => {
+  let playerDisplay = document.querySelector("#player-score span");
+  let machineDisplay = document.querySelector("#machine-score span");
 
-let game = ()=>{
-    startRound();
-    startRound();
-    startRound();
-    startRound();
-    startRound();
-
-    if(playerScore>machineScore){
-        console.log("The winner is the player");
-        console.log(`With a score of ${playerScore}`);
-        playerScore = 0;
-        machineScore = 0;
-    }else{
-        console.log("The winner is the computer");
-        console.log(`With a score of ${machineScore}`);
-        playerScore = 0;
-        machineScore = 0;
-    }
+  playerDisplay.textContent = playerScore;
+  machineDisplay.textContent = machineScore;
 }
+
+let game = () => {
+  let controller = document.querySelector(".controller");
+  let buttons = controller.childNodes;
+  document.querySelector("#player-score").style.color = "white";
+  document.querySelector("#machine-score").style.color = "white";
+  playerScore = 0;
+  machineScore = 0;
+  updateScore();
+  updateDisplay("nothing", "nothing");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      playerChoice = button.textContent;
+      if (playerScore != 5 && machineScore != 5) {
+        playRound(playerChoice.toLowerCase(), getComputerChoice());
+      }
+      else {
+        if (playerScore > machineScore) {
+          document.querySelector("#player-score").style.color = "Green";
+        } else {
+          document.querySelector("#machine-score").style.color = "Green";
+        }
+        return undefined;
+      }
+    })
+  })
+  if (playerScore >= 5 || machineScore >= 5) {
+    if (playerScore > machineScore) {
+      playerScore = 0;
+      machineScore = 0;
+    } else {
+      playerScore = 0;
+      machineScore = 0;
+    }
+  }
+}
+
+let startBtn = document.querySelector("#start")
+
+startBtn.addEventListener("click", () => {
+  game();
+})
